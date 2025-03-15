058c741b69792f481407d9f4a7e307dccf2bc41a
Arley <abenagil456@gmail.com>
2025-03-14 23:45:08 -0500

    creacion de componentes

---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <header className="App-header">
+        <h1>Burger Grill</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
